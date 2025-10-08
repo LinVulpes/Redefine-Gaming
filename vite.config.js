4cf7e4680defd3cae99db457a8bbb3bc5f54bcc1
linvulpes <thetpainghmu112@gmail.com>
2025-10-08 16:05:44 +0800

    Last commit is full development. This is for the deployment with GitHub

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Redefine-Gaming/',
   server: {
     watch: {
       usePolling: true, // Enable polling for file changes
